d73ba616b16351565a7011057ebbc85f08e6aa53
Anders Ingemann <aim@orbit.online>
2021-02-18 10:05:56 +0100

    rearrange things

--- docopt_parser/parser.py
+++ docopt_parser/parser.py
@@ -1,18 +1,20 @@
-from collections import namedtuple
 from docopt_parser import DocoptParseError
 import re
 from .parsec import ParseError, Parser, Value, eof, generate, many, many1, none_of, one_of, optional, regex, sepBy, sepBy1, string
+from collections import namedtuple
 
-def parse(doc):
-  try:
-    return docopt_lang.parse_strict(doc)
-  except ParseError as e:
-    raise DocoptParseError(explain_error(e, doc)) from None
-
-def explain_error(e, text):
-  line_no, col = e.loc_info(e.text, e.index)
-  line = text.split('\n')[line_no]
-  return '\n{line}\n{col}^\n{msg}'.format(line=line, col=' ' * col, msg=str(e))
+Multiple = namedtuple('Multiple', ['atom'])
+Long = namedtuple('Long', ['name', 'arg'])
+Short = namedtuple('Short', ['name', 'arg'])
+Shorts = namedtuple('Shorts', ['name', 'arg'])
+Command = namedtuple('Command', ['name'])
+Expression = namedtuple('Expression', ['select'])
+Optional = namedtuple('Optional', ['select'])
+Argument = namedtuple('Argument', ['name'])
+OptionsShortcut = namedtuple('OptionsShortcut', [])
+OptionLine = namedtuple('OptionLine', ['ident', 'default'])
+OptionLines = namedtuple('OptionLines', 'lines')
+DocoptLang = namedtuple('DocoptLang', ['usage', 'options'])
 
 def splat(constr):
   return lambda args: constr(*args)
@@ -48,7 +50,6 @@ def symbol_char(excludes):
 def symbol(excludes):
   return many1(symbol_char(excludes)).desc('symbol').parsecmap(''.join)
 
-Multiple = namedtuple('Multiple', ['atom'])
 multiple = string('...').desc('multiplier (...)')
 def multi(of):
   return optional(multiple).parsecmap(lambda m: Multiple(of) if m else of)
@@ -61,29 +62,24 @@ def atom(excludes):
     return a
   return p
 
-Long = namedtuple('Long', ['name', 'arg'])
 def long(excludes):
   return (string('--') >> symbol(excludes | string('=')) + optional(one_of(' =') >> arg)) \
     .desc('long option (--long)').parsecmap(splat(Long))
 
-Short = namedtuple('Short', ['name', 'arg'])
 def short(excludes):
   return (string('-') >> symbol_char(excludes | string('=')) + optional((space | string('=')) >> arg))\
     .desc('short option (-a)').parsecmap(splat(Short))
 
-Shorts = namedtuple('Shorts', ['name', 'arg'])
 def shorts(excludes):
   return (string('-') >> many1(symbol_char(excludes | string('='))) + optional((space | string('=')) >> arg))\
     .desc('short options (-abc)').parsecmap(splat(Shorts))
 
-Command = namedtuple('Command', ['name'])
 def command(excludes):
   return symbol(excludes).desc('command').parsecmap(Command)
 
 def seq(excludes):
   return sepBy(atom(excludes), exclude(whitespace, nl)).desc('sequence')
 
-Expression = namedtuple('Expression', ['select'])
 def expr(excludes):
   return sepBy(seq(excludes), either).desc('expression').parsecmap(Expression)
 
@@ -94,15 +90,13 @@ space = string(' ').desc('<space>')
 whitespace = regex(r'\s').desc('<whitespace>')
 indent = many1(space) | tab
 nl = string('\n').desc('<newline>')
+text = many1(exclude(any, regex(r'options:|usage:', re.I))).desc('Text').parsecmap(join_string)
 either = (many(space) >> string('|') << many(space)).desc('<pipe> (|)')
-Optional = namedtuple('Optional', ['select'])
 opt = (string('[') >> expr(one_of('| \n]')) << string(']')).desc('[optional]').parsecmap(Optional)
 group = (string('(') >> expr(one_of('| \n)')) << string(')')).desc('(group)').parsecmap(Expression)
 wrapped_arg = (string('<') + symbol(string('>')) + string('>')).desc('<ARG>').parsecmap(join_string)
 uppercase_arg = regex(r'[A-Z0-9][A-Z0-9-]+').desc('ARG')
-Argument = namedtuple('Argument', ['name'])
 arg = (wrapped_arg ^ uppercase_arg).desc('argument').parsecmap(Argument)
-OptionsShortcut = namedtuple('OptionsShortcut', [])
 options_shortcut = string('options').parsecmap(lambda x: OptionsShortcut())
 
 @generate
@@ -123,15 +117,10 @@ option_line_terminator = (nl + nl) ^ (nl + eof()) ^ (nl + indent + string('-'))
 option_doc = many1(exclude(any, option_default ^ option_line_terminator)).desc('option documentation')
 option_desc = optional(option_doc) >> optional(option_default) << optional(option_doc)
 
-OptionLine = namedtuple('OptionLine', ['ident', 'default'])
 option_line = (
   option_ident + optional(space + many1(space) >> option_desc)
 ).desc('option line').parsecmap(splat(OptionLine))
 
-text = many1(
-  exclude(any, regex(r'options:|usage:', re.I))).desc('Text').parsecmap(join_string)
-
-OptionLines = namedtuple('OptionLines', 'lines')
 option_lines = sepBy(option_line, nl + indent).parsecmap(OptionLines)
 usage_section = regex(r'usage:', re.I) >> optional(nl + indent) >> usage_lines << (eof() | nl)
 options_section = regex(r'options:', re.I) >> optional(nl + indent) >> option_lines << (eof() | nl)
@@ -142,9 +131,18 @@ def doc():
   usage = yield usage_section
   options += yield many(text ^ options_section)
   return usage, [o for o in options if isinstance(o, OptionLines)]
-DocoptLang = namedtuple('DocoptLang', ['usage', 'options'])
 docopt_lang = doc.parsecmap(splat(DocoptLang))
 
+def parse(doc):
+  try:
+    return docopt_lang.parse_strict(doc)
+  except ParseError as e:
+    raise DocoptParseError(explain_error(e, doc)) from None
+
+def explain_error(e, text):
+  line_no, col = e.loc_info(e.text, e.index)
+  line = text.split('\n')[line_no]
+  return '\n{line}\n{col}^\n{msg}'.format(line=line, col=' ' * col, msg=str(e))
 
 def ast_tostr(ast, indent=''):
   tree = ''
