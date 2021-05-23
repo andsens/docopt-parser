1b5e0c4f353dd7fb7c76505d6bbfa45320ac622d
Anders Ingemann <anders@ingemann.de>
2021-05-23 12:55:51 +0200

    Inline atom() in seq()

--- docopt_parser/nodes/usage/sequence.py
+++ docopt_parser/nodes/usage/sequence.py
@@ -3,13 +3,32 @@ from parsec import optional, generate, eof
 from .. import lookahead, either, whitespaces, nl
 
 def seq(options):
-  from .atom import atom
+  from .group import Group
+  from .optional import Optional
+  from .optionsshortcut import OptionsShortcut
+  from .argumentseparator import ArgumentSeparator
+  from .optionlist import OptionList
+  from ..argument import Argument
+  from .command import Command
+  from .multiple import Multiple
 
   @generate('sequence')
   def p():
     nodes = []
     while True:
-      nodes.extend((yield atom(options)))
+      atom = yield (
+        Group.group(options) | Optional.optional(options) | OptionsShortcut.shortcut
+        | ArgumentSeparator.separator | OptionList.options(options)
+        | Argument.arg | Command.command
+      ).desc('any element (cmd, ARG, options, --option, (group), [optional], --)')
+      if isinstance(atom, list):
+        # We're dealing with an optionlist, append all children to the sequence
+        # The "..." only affects the last option in the list
+        atom[-1] = yield Multiple.multi(atom[-1])
+        nodes.extend(atom)
+      else:
+        atom = yield Multiple.multi(atom)
+        nodes.append(atom)
       if (yield optional(whitespaces)) is None:
         break
       if (yield lookahead(optional(either | nl | eof()))) is not None:
