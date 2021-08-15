c0aa12abd1111ee92ffbb83e50ea9cf1e3a31d6b
Anders Ingemann <anders@ingemann.de>
2021-08-15 10:55:53 +0200

    Options in the options shortcut are *always* optional
    ... regardless of whether you reference them as "[options]" or "options"

--- docopt_parser/nodes/options_shortcut.py
+++ docopt_parser/nodes/options_shortcut.py
@@ -1,10 +1,11 @@
 from parsec import generate
 from .option import SpecSource
+from .optional import Optional
 from . import string
 
 def options_shortcut(options):
   @generate('options shortcut')
   def p():
     yield string('options')
-    return list(filter(lambda o: o.source == SpecSource.OPTIONS, options))
+    return Optional(list(filter(lambda o: o.source == SpecSource.OPTIONS, options)))
   return p
