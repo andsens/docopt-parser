dcab6fbde3bb73bab23fcd4a0720bdda06eeb3ce
Anders Ingemann <anders@ingemann.de>
2022-04-13 14:42:24 +0200

    doc: Fix unused option warning

--- docopt_parser/nodes/doc.py
+++ docopt_parser/nodes/doc.py
@@ -1,4 +1,7 @@
 from typing import Generator, Iterator, Union
+
+from .options_shortcut import OptionsShortcut
+from .choice import Choice
 from .command import Command
 from .argument import Argument
 from .argumentseparator import ArgumentSeparator
@@ -11,6 +14,8 @@ from .usage_section import UsageSection, section as usage_section
 from .option import Option
 from .optionref import OptionRef
 import logging
+import warnings
+from ordered_set import OrderedSet
 
 log = logging.getLogger(__name__)
 
@@ -26,31 +31,49 @@ def doc(strict: bool):
     ).parsecmap(flatten))))
     root = Doc(items)
     # merge_identical_leaves(root)
-    # validate_ununused_options(root)
+    validate_ununused_options(root)
     # mark_multiple(root)
-    # validate_unambiguous_options(options)
+    validate_unambiguous_options(root)
     return root
   return p
 
 class Doc(AstNode):
+  usage: UsageSection
+  option_sections: list[OptionsSection]
+
   def __init__(self, items: list[UsageSection, OptionsSection, str]):
     super().__init__(items)
     self.usage = next(filter(lambda n: isinstance(n, UsageSection), self.items), None)
-    self.options = list(filter(lambda n: isinstance(n, OptionsSection), self.items))
+    self.option_sections = list(filter(lambda n: isinstance(n, OptionsSection), self.items))
+
+  @property
+  def usage_options(self) -> list[Option]:
+    def get_opts(memo, node):
+      if isinstance(node, Option):
+        memo.add(node)
+      if isinstance(node, OptionRef):
+        memo.add(node.ref)
+      return memo
+    return self.usage.reduce(get_opts, OrderedSet())
+
+  @property
+  def section_options(self) -> list[Option]:
+    return OrderedSet(sum([o.items for o in self.option_sections], []))
 
   def __repr__(self) -> str:
     return f'''{self.indent(self.items, lvl=0)}'''
 
   def __iter__(self) -> Iterator[tuple[str, Union[str, list[dict]]]]:
     yield 'usage', dict(self.usage)
-    yield 'options', list(map(dict, self.options))
+    yield 'options', list(map(dict, self.option_sections))
 
 
-def validate_unambiguous_options(options):
+def validate_unambiguous_options(root: Doc):
+  options = root.section_options
   shorts = [getattr(o.short, 'name') for o in options if o.short is not None]
   longs = [getattr(o.long, 'name') for o in options if o.long is not None]
-  dup_shorts = set([n for n in shorts if shorts.count(n) > 1])
-  dup_longs = set([n for n in longs if longs.count(n) > 1])
+  dup_shorts = OrderedSet([n for n in shorts if shorts.count(n) > 1])
+  dup_longs = OrderedSet([n for n in longs if longs.count(n) > 1])
   messages = \
       ['-%s is specified %d times' % (n, shorts.count(n)) for n in dup_shorts] + \
       ['--%s is specified %d times' % (n, longs.count(n)) for n in dup_longs]
@@ -58,7 +81,7 @@ def validate_unambiguous_options(options):
     from .. import DocoptParseError
     raise DocoptParseError(', '.join(messages))
 
-def merge_identical_leaves(node, known_leaves=set()):
+def merge_identical_leaves(node, known_leaves=OrderedSet()):
   if isinstance(node, AstNode):
     for idx, item in enumerate(node.items):
       if isinstance(item, (Command, Argument, ArgumentSeparator)):
@@ -69,19 +92,13 @@ def merge_identical_leaves(node, known_leaves=set()):
       elif isinstance(item, AstNode):
         merge_identical_leaves(item, known_leaves)
 
-def validate_ununused_options(root):
-  def get_opts(memo, node):
-    if isinstance(node, Option):
-      memo.add(node)
-    if isinstance(node, OptionRef):
-      memo.add(node.ref)
-    return memo
-  usage_options = root.usage.reduce(get_opts, set())
-  options_section_options = root.options.reduce(get_opts, set())
-  unused_options = options_section_options - usage_options
+def validate_ununused_options(root: Doc) -> None:
+  if root.usage.reduce(lambda memo, node: memo or isinstance(node, OptionsShortcut), False):
+    return
+  unused_options = root.section_options - root.usage_options
   if len(unused_options) > 0:
     unused_list = '\n'.join(map(lambda o: f'* {o.ident}', unused_options))
-    log.warn(f'''{len(unused_options)} options are not referenced from the usage section:
+    warnings.warn(f'''{len(unused_options)} options are not referenced from the usage section:
 {unused_list}''')
 
 def mark_multiple(node, repeatable=False, siblings=[]):
