1b8333c0c27f97c012115a622d24a879d84ce2dd
Anders Ingemann <anders@ingemann.de>
2022-04-14 10:25:13 +0200

    post.sh: use mkdir `-p`

--- .devcontainer/post.sh
+++ .devcontainer/post.sh
@@ -2,14 +2,13 @@
 
 set -e
 
-mkdir "$HOME/.local"
-mkdir "$HOME/.local/bin"
+mkdir -p "$HOME/.local/bin"
 wget -O- https://github.com/koalaman/shellcheck/releases/download/v0.7.1/shellcheck-v0.7.1.linux.x86_64.tar.xz | \
   tar -xJC "$HOME/.local/bin" --strip-components 1 shellcheck-v0.7.1/shellcheck
 
 pip --disable-pip-version-check --no-cache-dir install --user poetry
 
-mkdir "$HOME/.config/pypoetry"
+mkdir -p "$HOME/.config/pypoetry"
 cp .devcontainer/poetry-config.toml "$HOME/.config/pypoetry/config.toml"
 
 poetry install
