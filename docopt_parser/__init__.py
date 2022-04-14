8f0559e5962f7e0501c7a64ff32b78907a94d112
Anders Ingemann <anders@ingemann.de>
2022-04-14 10:25:46 +0200

    Include __version__ in exports

--- docopt_parser/__init__.py
+++ docopt_parser/__init__.py
@@ -3,10 +3,10 @@ docopt-parser - A parsing library for the docopt helptext
 """
 from docopt_parser.doc import parse, DocoptParseError
 
-__all__ = ['parse', 'DocoptParseError']
-
 try:
   # pyright: reportMissingImports=false
   from .version import __version__
 except ImportError:
   __version__ = '0.0.0-dev'
+
+__all__ = ['parse', 'DocoptParseError', '__version__']
