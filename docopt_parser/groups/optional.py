445c10f95eaa9f2466131c243dc57009f071f356
Anders Ingemann <anders@ingemann.de>
2022-04-15 15:17:56 +0200

    Unnest [options]

--- docopt_parser/groups/optional.py
+++ docopt_parser/groups/optional.py
@@ -1,7 +1,7 @@
 from typing import List
 from parsec import generate
 
-from docopt_parser import base, groups, parsers
+from docopt_parser import base, elements, groups, parsers
 from docopt_parser.base.astleaf import AstLeaf
 from docopt_parser.helpers import GeneratorParser
 
@@ -19,11 +19,11 @@ class Optional(base.AstNode):
     yield 'items', [dict(item) for item in self.items]
 
 @generate('[optional]')
-def optional() -> GeneratorParser[Optional | None]:
+def optional() -> GeneratorParser[Optional | elements.OptionsShortcut | None]:
   node: AstLeaf = yield (parsers.char('[') >> groups.choice << parsers.char(']'))
-  # Unnest [[optional]], or [sequence]
-  if isinstance(node, (Optional)):
-    return Optional(node.items)
+  # Unnest [[optional]], or [options]
+  if isinstance(node, (Optional, elements.OptionsShortcut)):
+    return node
   elif node is None:
     return None
   else:
