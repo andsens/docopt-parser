7a33785cf44d46a1ab3f9016895f87170c5200ca
Anders Ingemann <aim@orbit.online>
2022-10-16 13:06:12 +0200

    .devcontainer: Considerably improve dev experience
    The virtualenv now integrates a lot better with vscode

--- .devcontainer/post.sh
+++ .devcontainer/post.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
-
 set -e
 
 pip --disable-pip-version-check --no-cache-dir install --user poetry
-
 mkdir -p "$HOME/.config/pypoetry"
 cp .devcontainer/poetry-config.toml "$HOME/.config/pypoetry/config.toml"
-
-poetry install
+poetry install --no-root
